1cca009449bd4f223ac0d18d7bb045a4fab7ca3e
Himal Subedi <itissubedi@gmail.com>
2025-08-03 15:52:29 +0545

    Initial Commit

---
+++ backend/controllers/recommendController.js
@@ -0,0 +1,43 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = process.env.PAGINATION_LIMIT;
+  const page = Number(req.body.pageNumber) || 1;
+  console.log(req.body.cartItems);
+
+  const keyword = req.body.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
+});
+
+
+
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
+export {
+  getProducts,
+  getTopProducts,
+};
