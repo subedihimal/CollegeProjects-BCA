806b66558a398cbd29d159caf86db3ffb3dba16c
Himal Subedi <itissubedi@gmail.com>
2025-08-14 11:09:11 +0545

    Added COD

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -17,7 +17,7 @@ const PaymentScreen = () => {
     }
   }, [navigate, shippingAddress]);
 
-  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+  const [paymentMethod, setPaymentMethod] = useState('COD');
 
   const dispatch = useDispatch();
 
@@ -38,10 +38,10 @@ const PaymentScreen = () => {
             <Form.Check
               className='my-2'
               type='radio'
-              label='PayPal or Credit Card'
-              id='PayPal'
+              label='Cash On Delivery'
+              id='COD'
               name='paymentMethod'
-              value='PayPal'
+              value='COD'
               checked
               onChange={(e) => setPaymentMethod(e.target.value)}
             ></Form.Check>
