a1d550fe5fb1a063276ac3020078aa63119929f1
subedihimal <itissubedi@gmail.com>
2026-08-19 20:09:45 +0545

    feat: mobile view support

--- 8-PuzzleGameandSolver/navbar/navbar.php
+++ 8-PuzzleGameandSolver/navbar/navbar.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="/navbar/navbar.css">
 <nav class="navbar">
+    <div class="navbar-title">8 Puzzle Game and Solver</div>
     <div class="logout">
-        <form action="/login/logout.php" method="POST" style="display: inline;">
-            <button class="logout-btn" type="submit" style="background: none; border: none; cursor: pointer; padding: 0;">
-                <img src="/navbar/icon/logout.png" alt="Logout" style="height: 40px; width: 40px;" />
+        <form action="/login/logout.php" method="POST">
+            <button class="logout-btn" type="submit" aria-label="Log out">
+                <img src="/navbar/icon/logout.png" alt="" />
             </button>
         </form>
     </div>
-    <div class="navbar-title">8 Puzzle Game and Solver</div>
 </nav>
